dd6594bc657bbbc695b61e8fb6267e09661aaf97
Begana Choi <begana001.jpg@gmail.com>
2020-02-24 08:31:08 +0100

    6

---
+++ interactive-form-v2/js/script.js
@@ -0,0 +1,102 @@
+console.log('added well');
+const name = document.querySelector('#name');
+name.focus();
+
+const otherTitle = document.querySelector('#other-title');
+otherTitle.style.display="none";
+
+const designTheme = document.querySelector('#design option');
+designTheme.hidden = true;
+
+const tshirtTheme = document.querySelector('#design option');
+tshirtTheme.hidden = true;
+
+const colorSelect = document.querySelector('#color');
+const colorTheme = document.createElement('option');
+colorTheme.textContent = "Please select a T-shirt theme";
+colorSelect.appendChild(colorTheme);
+colorTheme.selected = true;
+
+
+const colors = document.querySelectorAll('#color option');
+for ( let i = 0; i < colors.length; i ++ ){
+    colors[i].hidden = true;
+}
+
+const designSelect = document.querySelector('#design');
+const jsPuns = document.querySelector('#design option[value="js puns"]');
+const heartJs = document.querySelector('#design option[value="heart js"]');
+
+const jsPunsColor1 = document.querySelector('#color option[value="cornflowerblue"]');
+const jsPunsColor2 = document.querySelector('#color option[value="darkslategrey"]');
+const jsPunsColor3 = document.querySelector('#color option[value="gold"]');
+
+const heartJsColor1 = document.querySelector('#color option[value="tomato"]');
+const heartJsColor2 = document.querySelector('#color option[value="steelblue"]');
+const heartJsColor3 = document.querySelector('#color option[value="dimgrey"]');
+
+designSelect.addEventListener('change', () => {
+    if( jsPuns.selected === true ){
+        jsPunsColor1.hidden = false;
+        jsPunsColor2.hidden = false;
+        jsPunsColor3.hidden = false;
+        heartJsColor1.hidden = true;
+        heartJsColor2.hidden = true;
+        heartJsColor3.hidden = true;
+    }
+    if( heartJs.selected === true ){
+        heartJsColor1.hidden = false;
+        heartJsColor2.hidden = false;
+        heartJsColor3.hidden = false;
+        jsPunsColor1.hidden = true;
+        jsPunsColor2.hidden = true;
+        jsPunsColor3.hidden = true;
+    }  
+});
+
+const activities = document.querySelector('.activities');
+const element = document.createElement('element');
+activities.appendChild(element);
+let totalCost = 0;
+
+activities.addEventListener('change', (e) => {
+
+    const inputClicked = event.target;
+    const inputAtt = inputClicked.getAttribute('data-cost');
+    const inputCost = parseInt(inputAtt);
+    
+
+    if( inputClicked.checked ){
+        totalCost += inputCost;
+    } else {
+        totalCost -= inputCost;
+    }
+    element.innerHTML = "Total : $" + totalCost;
+
+    const dayAndTime = inputClicked.getAttribute('data-day-and-time');
+    const checkboxes = document.querySelectorAll('.activities input');
+
+    for ( let i = 0; i < checkboxes.length; i ++ ){
+        const currentCheckbox = checkboxes[i].getAttribute('data-day-and-time');
+        if( dayAndTime === currentCheckbox && inputClicked !== checkboxes[i] ){
+            if( inputClicked.checked ){
+                checkboxes[i].disabled = true;
+            } else {
+                checkboxes[i].disabled = false;
+            }
+        }
+    }
+});
+
+const paymentMethod = document.querySelector('#payment option[value="select method"]');
+paymentMethod.hidden = true;
+
+const creditcard = document.querySelector('#credit-card');
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+
+const creditcardOption = document.querySelector('#payment option[value="credit card"]');
+console.log(creditcardOption);
+if ( creditcardOption.selected === true ){
+    paypal.style.display = 'none';
+}
